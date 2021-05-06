3d66b5ec70884b8ece891eea054f956fe7c56b15
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-05-06 22:19:55 +0100

    Implemented JoinProtocol and LeaveProtocol

---
+++ src/main/java/sdis/Protocols/SystemStorage/LeaveProtocol.java
@@ -0,0 +1,36 @@
+package sdis.Protocols.SystemStorage;
+
+import sdis.Chord;
+import sdis.Peer;
+import sdis.Protocols.Chord.FingersRemoveProtocol;
+import sdis.Protocols.Chord.UpdatePredecessorProtocol;
+import sdis.Protocols.ProtocolSupplier;
+
+public class LeaveProtocol extends ProtocolSupplier<Void> {
+    private final Peer peer;
+    private final ProtocolSupplier<Void> moveKeys;
+
+    public LeaveProtocol(Peer peer, ProtocolSupplier<Void> moveKeys){
+        this.peer = peer;
+        this.moveKeys = moveKeys;
+    }
+
+    @Override
+    public Void get() {
+        Chord chord = peer.getChord();
+        long r = chord.getKey();
+
+        // Update predecessors and fingers tables of other nodes
+        // Update predecessor of successor
+        UpdatePredecessorProtocol updatePredecessorProtocol = new UpdatePredecessorProtocol(chord, chord.getPredecessor());
+        updatePredecessorProtocol.get();
+        // Update other nodes' fingers tables
+        FingersRemoveProtocol fingersRemoveProtocol = new FingersRemoveProtocol(chord);
+        fingersRemoveProtocol.get();
+
+        // Move keys
+        moveKeys.get();
+
+        return null;
+    }
+}
