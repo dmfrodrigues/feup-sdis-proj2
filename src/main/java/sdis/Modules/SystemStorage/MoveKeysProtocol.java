229e1a9ed585cc9ac8d9a71d8099f8739b75ed3d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-05-11 12:54:46 +0100

    Implemented MoveKeys protocol

---
+++ src/main/java/sdis/Modules/SystemStorage/MoveKeysProtocol.java
@@ -0,0 +1,42 @@
+package sdis.Modules.SystemStorage;
+
+import sdis.Modules.Chord.Chord;
+import sdis.Modules.DataStorage.DataStorage;
+import sdis.Modules.DataStorage.Messages.GetMessage;
+import sdis.Modules.ProtocolSupplier;
+import sdis.Modules.SystemStorage.Messages.MoveKeysMessage;
+import sdis.UUID;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+
+public class MoveKeysProtocol extends ProtocolSupplier<Void> {
+
+    private final SystemStorage systemStorage;
+    private final Chord.NodeInfo nodeInfo;
+
+    public MoveKeysProtocol(SystemStorage systemStorage, Chord.NodeInfo nodeInfo){
+        this.systemStorage = systemStorage;
+        this.nodeInfo = nodeInfo;
+    }
+
+    @Override
+    public Void get() {
+        Chord chord = systemStorage.getChord();
+        Chord.NodeInfo r = chord.getNodeInfo();
+
+        Chord.NodeInfo s = chord.getSuccessor();
+        try{
+            MoveKeysMessage moveKeysMessage = new MoveKeysMessage(r);
+            Socket socket = systemStorage.send(s.address, moveKeysMessage);
+            socket.shutdownOutput();
+            socket.getInputStream().readAllBytes();
+            socket.close();
+            return null;
+        } catch (IOException e) {
+            throw new CompletionException(e);
+        }
+    }
+}
--- src/main/java/sdis/Modules/SystemStorage/SystemStorage.java
+++ src/main/java/sdis/Modules/SystemStorage/SystemStorage.java
@@ -2,6 +2,13 @@ package sdis.Modules.SystemStorage;
 
 import sdis.Modules.Chord.Chord;
 import sdis.Modules.DataStorage.DataStorage;
+import sdis.Modules.SystemStorage.Messages.MoveKeysMessage;
+import sdis.Modules.SystemStorage.Messages.SystemStorageMessage;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 public class SystemStorage {
     private final Chord chord;
@@ -18,5 +25,13 @@ public class SystemStorage {
 
     public DataStorage getDataStorage() {
         return dataStorage;
+    }
+
+    public Socket send(InetSocketAddress to, SystemStorageMessage m) throws IOException {
+        Socket socket = new Socket(to.getAddress(), to.getPort());
+        OutputStream os = socket.getOutputStream();
+        os.write(m.asByteArray());
+        os.flush();
+        return socket;
     }
 }
