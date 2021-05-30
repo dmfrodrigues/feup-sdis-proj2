0b48c3f708fcfdb2381d5e66a296b532501f4b5e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-05-31 00:46:59 +0100

    Added tests

--- src/test/java/sdis/Modules/Main/MainJoinLeaveTest.java
+++ src/test/java/sdis/Modules/Main/MainJoinLeaveTest.java
@@ -168,16 +168,83 @@ public class MainJoinLeaveTest {
         assertTrue(peer2.leave());
     }
 
-    @Ignore
     @Test(timeout=10000)
     public void die_2peers() throws Exception {
         int KEY_SIZE = 10;
 
         Peer peer1 = new Peer(KEY_SIZE, 0, InetAddress.getByName("localhost"), Paths.get("bin"));
         assertTrue(peer1.join());
+
+        Peer peer2 = new Peer(KEY_SIZE, 800, InetAddress.getByName("localhost"), Paths.get("bin"));
+        peer2.join(peer1.getSocketAddress());
+
+        Username username = new Username("user1");
+        Password password = new Password("1234");
+        Main.Path path = new Main.Path("mydata");
+        byte[] data = "my data".getBytes();
+        ChunkIterator chunkIterator = new ByteArrayChunkIterator(data, Main.CHUNK_SIZE);
+
+        assertEquals(  0, peer2.getChord().getFingerRaw(0).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(1).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(2).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(3).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(4).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(5).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(6).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(9).key.toLong());
+
+        assertTrue(peer1.backup(username, password, path, 1, chunkIterator));
+        assertTrue(peer1.die());
+
+        assertEquals(  0, peer2.getChord().getFingerRaw(0).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(1).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(2).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(3).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(4).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(5).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(6).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(9).key.toLong());
+
+        assertEquals(800, peer2.getChord().getFingerInfo(0).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(1).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(2).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(3).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(4).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(5).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(6).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerInfo(9).key.toLong());
+
+        assertEquals(800, peer2.getChord().getFingerRaw(0).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(1).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(2).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(3).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(4).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(5).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(6).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(9).key.toLong());
+
+        assertTrue(peer2.leave());
+    }
+
+
+    @Ignore
+    @Test(timeout=10000)
+    public void die_2peers_2() throws Exception {
+        int KEY_SIZE = 10;
+
+        Peer peer1 = new Peer(KEY_SIZE, 0, InetAddress.getByName("localhost"), Paths.get("bin"));
+        assertTrue(peer1.join());
         DataStorage dataStorage1 = peer1.getDataStorage();
 
-        Peer peer2 = new Peer(KEY_SIZE, 500, InetAddress.getByName("localhost"), Paths.get("bin"));
+        Peer peer2 = new Peer(KEY_SIZE, 800, InetAddress.getByName("localhost"), Paths.get("bin"));
         peer2.join(peer1.getSocketAddress());
         DataStorage dataStorage2 = peer2.getDataStorage();
 
@@ -188,32 +255,31 @@ public class MainJoinLeaveTest {
         ChunkIterator chunkIterator = new ByteArrayChunkIterator(data, Main.CHUNK_SIZE);
 
         assertTrue(peer1.backup(username, password, path, 1, chunkIterator));
-
-        assertNotNull(dataStorage1.get(new UUID("user1-0-1")));
-        assertNotNull(dataStorage1.get(new UUID("user1-0-2")));
-        assertNotNull(dataStorage1.get(new UUID("user1-0-3")));
-        assertNotNull(dataStorage1.get(new UUID("user1-0-5")));
-        assertNotNull(dataStorage1.get(new UUID("user1-0-6")));
-        assertNotNull(dataStorage1.get(new UUID("user1-0-9")));
-        assertNotNull(dataStorage1.get(new UUID("user1/mydata-0-0")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-0")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-4")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-7")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-8")));
-
         assertTrue(peer1.die());
 
-        assertNotNull(dataStorage2.get(new UUID("user1-0-0")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-1")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-2")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-3")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-4")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-5")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-6")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-7")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-8")));
-        assertNotNull(dataStorage2.get(new UUID("user1-0-9")));
-        assertNotNull(dataStorage2.get(new UUID("user1/mydata-0-0")));
+        assertEquals(  0, peer2.getChord().getFingerRaw(0).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(1).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(2).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(3).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(4).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(5).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(6).key.toLong());
+        assertEquals(  0, peer2.getChord().getFingerRaw(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(9).key.toLong());
+
+//        assertTrue(peer2.synchronize());
+
+        assertEquals(800, peer2.getChord().getFingerRaw(0).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(1).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(2).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(3).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(4).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(5).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(6).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(7).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(8).key.toLong());
+        assertEquals(800, peer2.getChord().getFingerRaw(9).key.toLong());
 
         assertTrue(peer2.leave());
     }
